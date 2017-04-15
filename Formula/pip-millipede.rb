92ff9aebb385d9de6d5aef14f096b90235424edd
rockyluke <rockyluke@offline.net>
2017-04-15 22:58:28 +0200

    Update Formula

--- Formula/pip-millipede.rb
+++ Formula/pip-millipede.rb
@@ -7,11 +7,15 @@ class PipMillipede < Formula
   version "2.0"
   homepage "https://github.com/getmillipede/millipede-python/"
   url "https://github.com/getmillipede/millipede-python/releases/download/2.0/millipede-#{version}.tar.gz"
-  sha256 ""
+  sha256 "009466d0a919af3dc4afadb6a70efb5610dab96d7134f18ac9f3a37c4279079d"
 
   def install
     venv = virtualenv_create(libexec)
     venv.pip_install_and_link buildpath
+  end
+
+  test do
+    system "#{bin}/millipede --help"
   end
 end
 # EOF
--- Formula/pip-pylint.rb
+++ Formula/pip-pylint.rb
@@ -7,11 +7,15 @@ class PipPylint < Formula
   version "1.7.0"
   homepage "https://github.com/pycqa/pylint/"
   url "https://github.com/pycqa/pylint/archive/pylint-#{version}.tar.gz"
-  sha256 ""
+  sha256 "a3e67cb0610bc9e648235c1790d030bbf973c54138f13c1f076e7f2be693dfe1"
 
   def install
     venv = virtualenv_create(libexec)
     venv.pip_install_and_link buildpath
+  end
+
+  test do
+    system "#{bin}/pylint --help"
   end
 end
 # EOF
