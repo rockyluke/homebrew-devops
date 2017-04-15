23dd0596e98028c2f26dc9e0b0295e3a6e0b0c94
rockyluke <rockyluke@offline.net>
2017-04-15 23:17:43 +0200

    Update Formula

--- Formula/pip-pylint.rb
+++ Formula/pip-pylint.rb
@@ -11,17 +11,23 @@ class PipPylint < Formula
 
   resource "astroid" do
     url "https://github.com/timothycrosley/isort/archive/4.2.5.tar.gz"
+    sha256 "d055e9697c3d12e2f9a67691853f19b39d694a6bc533ebcd21c5e609d67c597f"
+  end
+
+  resource "configparser" do
+    url "https://pypi.python.org/packages/7c/69/c2ce7e91c89dc073eb1aa74c0621c3eefbffe8216b3f9af9d3885265c01c/configparser-3.5.0.tar.gz"
     sha256 ""
   end
 
   resource "isort" do
     url "https://github.com/timothycrosley/isort/archive/4.2.5.tar.gz"
-    sha256 ""
+    sha256 "d055e9697c3d12e2f9a67691853f19b39d694a6bc533ebcd21c5e609d67c597f"
   end
 
   def install
     venv = virtualenv_create(libexec)
     venv.pip_install resource("astroid")
+    venv.pip_install resource("configparser")
     venv.pip_install resource("isort")
     venv.pip_install_and_link buildpath
   end
