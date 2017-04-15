363e8f8835b8b8f61d75ab0eeb5e2927362f2c0f
rockyluke <rockyluke@offline.net>
2017-04-16 00:31:51 +0200

    Update Formula

--- Formula/pip-whereami.rb
+++ Formula/pip-whereami.rb
@@ -9,8 +9,6 @@ class PipWhereami < Formula
   url "https://pypi.python.org/packages/d4/96/4da7c4f7655f399631e2b67d99fd9fff01b0998b5af9887ad4455d2384b4/whereami-#{version}.tar.gz"
   sha256 "8ff8cbd876fc3817ba69a8caf16233401493e6f9ddd0f2e1bd9eb5838cb0fb5a"
 
-  depends_on :python3
-
   resource "access_points" do
     url "https://pypi.python.org/packages/69/15/b71bbe5fe766ba322f7bbb7a1e417c6e80a61fddeb60543965e935aae31c/access_points-0.3.52.tar.gz"
     sha256 "e5e872b276d3642a5f0f9214aee4f53839ec9ff5d06f56da74a1a372305fba6e"
