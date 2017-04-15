36235f01d58a9d063a96fe8edc96e17fe1a901e6
rockyluke <rockyluke@offline.net>
2017-04-15 23:40:47 +0200

    Update Formula

--- Formula/pip-pylint.rb
+++ Formula/pip-pylint.rb
@@ -9,21 +9,11 @@ class PipPylint < Formula
   url "https://github.com/pycqa/pylint/archive/pylint-#{version}.tar.gz"
   sha256 "a3e67cb0610bc9e648235c1790d030bbf973c54138f13c1f076e7f2be693dfe1"
 
-  depends_on :python
+  depends_on :python3
 
   resource "astroid" do
     url "https://github.com/timothycrosley/isort/archive/4.2.5.tar.gz"
     sha256 "d055e9697c3d12e2f9a67691853f19b39d694a6bc533ebcd21c5e609d67c597f"
-  end
-
-  resource "backports.functools_lru_cache" do
-    url "https://pypi.python.org/packages/00/9c/70c865c629d406531d05f46a5c5f68874f186bcc0424ed3bd7cbe54bfe7d/backports.functools_lru_cache-1.3.tar.gz"
-    sha256 "cd63e58510a2aa0b020595bff195e3c269dd0926dc631c0a2e45a092906aee3c"
-  end
-
-  resource "configparser" do
-    url "https://pypi.python.org/packages/7c/69/c2ce7e91c89dc073eb1aa74c0621c3eefbffe8216b3f9af9d3885265c01c/configparser-3.5.0.tar.gz"
-    sha256 "5308b47021bc2340965c371f0f058cc6971a04502638d4244225c49d80db273a"
   end
 
   resource "isort" do
@@ -34,8 +24,6 @@ class PipPylint < Formula
   def install
     venv = virtualenv_create(libexec)
     venv.pip_install resource("astroid")
-    venv.pip_install resource("backports.functools_lru_cache")
-    venv.pip_install resource("configparser")
     venv.pip_install resource("isort")
     venv.pip_install_and_link buildpath
   end
