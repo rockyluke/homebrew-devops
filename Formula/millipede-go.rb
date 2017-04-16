bf8f628e0b7ca4c5736d1e0848b882bfc859121f
rockyluke <rockyluke@offline.net>
2017-04-16 03:21:59 +0200

    Update Formula

--- Formula/millipede-go.rb
+++ Formula/millipede-go.rb
@@ -5,7 +5,7 @@ require 'language/go'
 class MillipedeGo < Formula
   desc 'Print a beautiful millipede.'
   version '1.3.0'
-  homepage 'https://github.com/getmillipede/millipede-go'
+  homepage 'https://github.com/getmillipede/millipede-go/'
   url "https://github.com/getmillipede/millipede-go/archive/v#{version}.tar.gz"
   sha256 '49d1e6ee6843b82d6b72254d00813941590ad9d4850b27a60e2fc10cfdd9c4f4'
 
