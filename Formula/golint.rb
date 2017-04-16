f5781556e826963fef3a53d1ee86c0b9e140f84a
rockyluke <rockyluke@offline.net>
2017-04-16 11:37:38 +0200

    Update Formula

--- Formula/golint.rb
+++ Formula/golint.rb
@@ -7,18 +7,23 @@ class Golint < Formula
   version 'master'
   homepage 'https://github.com/golang/lint/'
   url 'https://github.com/golang/lint/archive/master.tar.gz'
-  sha256 ''
+  sha256 '0605da09ffae441b843764a9d7b0ffc1e3bdba3b46da91bcd75ff9b5257bc194'
 
   depends_on 'go' => :build
+
+  go_resource 'golang.org/x/tools/' do
+    url 'https://go.googlesource.com/tools.git'
+  end
 
   def install
     ENV['GOPATH']      = buildpath
     ENV['GOBIN']       = buildpath
     ENV['CGO_ENABLED'] = '0'
 
+    Language::Go.stage_deps resources, buildpath/'src'
     (buildpath/'src/github.com/golang/lint/').install Dir['*']
 
-    system 'go', 'build', '-o', "#{bin}/golint", '-v', 'github.com/golang/lint/golint/'
+    system 'go', 'build', '-o', "#{bin}/golint", 'github.com/golang/lint/golint/'
   end
 
   test do
