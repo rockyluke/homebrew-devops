86ca78356183dfe57d986fa6370735e333209cad
rockyluke <rockyluke@offline.net>
2017-04-16 11:29:19 +0200

    Add golinte

---
+++ Formula/golint.rb
@@ -0,0 +1,28 @@
+# -*- ruby -*-
+
+require 'language/go'
+
+class Golint < Formula
+  desc 'This is a linter for Go source code.'
+  version 'master'
+  homepage 'https://github.com/golang/lint/'
+  head 'https://github.com/golang/lint.git'
+  sha256 ''
+
+  depends_on 'go' => :build
+
+  def install
+    ENV['GOPATH']      = buildpath
+    ENV['GOBIN']       = buildpath
+    ENV['CGO_ENABLED'] = '0'
+
+    (buildpath/'src/github.com/golang/lint/').install Dir['*']
+
+    system 'go', 'build', '-o', "#{bin}/golint", '-v', 'github.com/golang/lint/golint/'
+  end
+
+  test do
+    system "#{bin}/golint --help"
+  end
+end
+# EOF
