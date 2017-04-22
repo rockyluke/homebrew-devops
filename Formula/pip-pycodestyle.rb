34d9a1fdbde597dc077d0174747dc874e1add33d
rockyluke <rockyluke@offline.net>
2017-04-22 23:29:30 +0200

    Add pycodestyle from @PyCQA

---
+++ Formula/pip-pycodestyle.rb
@@ -0,0 +1,21 @@
+# -*- ruby -*-
+
+class PipPycodestyle < Formula
+  include Language::Python::Virtualenv
+
+  desc "Linter for Python files."
+  homepage "https://github.com/PyCQA/pycodestyle/"
+  url "https://pypi.python.org/packages/e1/88/0e2cbf412bd849ea6f1af1f97882add46a374f4ba1d2aea39353609150ad/pycodestyle-2.3.1.tar.gz"
+  sha256 "682256a5b318149ca0d2a9185d365d8864a768a28db66a84a2ea946bcc426766"
+
+  depends_on :python3
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/pycodestyle", "--help"
+  end
+end
+# EOF
